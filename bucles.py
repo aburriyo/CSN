56c2401d4627cb1bda43380e6f76c4e3540b4211
Bastián Chávez <bchavezo@usm.cl>
2026-03-19 13:00:21 +0000

    commit

---
+++ bucles.py
@@ -0,0 +1,7 @@
+estudiante={"nombre":"Bastián", "edad":23, "color_favorito":"Verde"}
+
+print(estudiante.keys())
+print("------------")
+print(estudiante.items())
+print("-------------")
+print(estudiante.values())
